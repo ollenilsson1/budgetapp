475b1b7cb49e15873d6bdc90a4ac9e49a7de6852
Olle Nilsson <[olle.nilsson@medieinstitutet.se]>
2020-11-20 16:44:30 +0100

    added income and expense total and more css

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 let countTotal = [] ;
+let countIncomeTotal = [] ;
+let countExpenseTotal = [] ;
 
 const incomeList = document.getElementById("income_list");
 const expenseList = document.getElementById("expense_list");
 const totalResult = document.getElementById("total_result");
+const incomeTotal = document.getElementById("income_total");
+const expenseTotal = document.getElementById("expense_total");
 
 function budget (){
     const selector = document.getElementById("add_select").value;
@@ -11,31 +15,45 @@ function budget (){
         const amount = document.getElementById("add_amount").value;
         let num1 = Number(amount);
         countTotal.push(num1);
+        //för att pusha till incometotal
+        countIncomeTotal.push(num1);
 
         const incomeP = document.createElement("p");
         incomeP.setAttribute("class", "income-p")
-        incomeP.innerHTML = text + "    " + amount;
+        incomeP.innerHTML = text + "    " + amount + "  " + "sek";
         incomeList.appendChild(incomeP);
-        console.log(countTotal);
     }  
     else if (selector == "-") {
         const text = document.getElementById("add_text").value;
         const amount = document.getElementById("add_amount").value;
         let num2 = Number(-amount);
         countTotal.push(num2);
+        countExpenseTotal.push(num2);
         const expenseP = document.createElement("p");
         expenseP.setAttribute("class" , "expense-p")
-        expenseP.innerHTML = text + "    " + amount;
+        expenseP.innerHTML = text + "    " + amount + "  " + "sek";
         expenseList.appendChild(expenseP);
-        console.log(countTotal);
     }
+    //for loop för att visa budgetens total
     let total = 0;
     for (i = 0; i<countTotal.length; i++)
     total += countTotal[i];
     totalResult.innerText = total;
-    console.log(total);
 
-    
+    //for loop för att visa totalen av alla inkomster
+    let totalIncome = 0;
+    for (i = 0; i<countIncomeTotal.length; i++)
+    totalIncome += countIncomeTotal[i];
+    incomeTotal.innerText = "Totala inkomster" + "  :  " + totalIncome + "  " + "sek";
+
+    //for loop för att visa totalen av alla utgifter
+    let totalExpense = 0;
+    for (i = 0; i<countExpenseTotal.length; i++)
+    totalExpense += countExpenseTotal[i];
+    expenseTotal.innerText = "Totala utgifter" + "  :  " + totalExpense + "  " + "sek";
+
+
+
 }
 
 // submit knapp som startar budget function.
@@ -44,14 +62,17 @@ const addBtn = document.getElementById("submit");
 addBtn.addEventListener("click" , budget);
 
 //remove function och delete knapp som rensar alla div och nollställer array.
-//FIXA INPUT FÄLTEN OCKSÅ
 function remove () {
     expenseList.innerHTML = "";
     incomeList.innerHTML = "";
     totalResult.innerText = "";
+    incomeTotal.innerText = "Totala inkomster" + "  :  "
+    expenseTotal.innerText = "Totala utgifter" + "  :  " 
     document.getElementById("add_text").value = "";
     document.getElementById("add_amount").value = "";
     countTotal.length = 0;
+    countIncomeTotal.length = 0;
+    countExpenseTotal.length = 0;
 }
 
 
